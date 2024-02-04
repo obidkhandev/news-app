083a191dd02846b0933568b9804d42877e52b189
@alwaysflutter <obidkhandev@gmail.com>
2024-02-04 15:50:00 +0500

    news descriptions page

--- lib/pages/new_descriptions.dart
+++ lib/pages/new_descriptions.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/compotations/my_app_bar.dart';
-import 'package:news_app/compotations/my_image.dart';
+// import 'package:news_app/compotations/my_image.dart';
 
 class NewsDescriptions extends StatefulWidget {
   const NewsDescriptions({super.key});
@@ -37,8 +36,85 @@ class _NewsDescriptionsState extends State<NewsDescriptions> {
               ),
             ),
           ),
+          const Padding(
+            padding: EdgeInsets.all(12),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/profile.png"),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  "By Andy Corbley",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "1hr ago",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                RowIconsAndText(
+                  icon: Icons.chat_bubble_outline,
+                  text: "8 commets",
+                ),
+                RowIconsAndText(
+                  icon: Icons.favorite_border,
+                  text: "34 likes",
+                ),
+                RowIconsAndText(
+                  icon: Icons.share,
+                  text: "Share",
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Text(
+              """
+San Pedro is a composite volcano in northern Chile and one of the tallest active volcanoes in the world. It is part of the Andean Volcanic Belt and, like other Andean volcanoes, was formed by the subduction of the Nazca Plate beneath the South America Plate. San Pedro is formed of two separate edifices, the Old Cone and the Young Cone, and is adjoined to a neighbouring volcano, San Pablo. The Old Cone was active over one hundred thousand years ago and was eventually truncated by a giant landslide that removed its northwestern side. Within the landslide scar lava flows and pyroclastic flows constructed the Young Cone as well as the lateral centre La Poruña. Some eruptions have been reported during historical time, and presently the volcano is fumarolically active. This photograph shows San Pedro in the foreground, with San Pablo visible behind it to the right.
+""",
+              style: TextStyle(fontSize: 16),
+            ),
+          )
         ],
       ),
+    );
+  }
+}
+
+class RowIconsAndText extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  const RowIconsAndText({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon),
+        const SizedBox(width: 10),
+        Text(
+          text,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
